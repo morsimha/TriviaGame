846dfa7d137b5508da96835071af2b58c47719b0
morsimha <mor.simha@intel.com>
2022-12-08 21:36:50 +0200

    fx loaded

--- src/main/java/com/example/triviagame/TriviaGameController.java
+++ src/main/java/com/example/triviagame/TriviaGameController.java
@@ -1,23 +1,37 @@
 package com.example.triviagame;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 import java.io.FileNotFoundException;
 
 public class TriviaGameController {
     @FXML
-    private Label welcomeText;
+    private RadioButton ansA;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private RadioButton ansB;
+
+    @FXML
+    private RadioButton ansC;
+
+    @FXML
+    private RadioButton ansD;
+
+    @FXML
+    private Button AnsBtn;
 
     @FXML
     private Canvas cnvs;
+
+    @FXML
+    private Label questionLabel;
 
     private GraphicsContext gc;
     private Questions que;
@@ -26,6 +40,7 @@ public class TriviaGameController {
     private int drawCounter;
     private boolean finished;
     private boolean win;
+    final ToggleGroup group = new ToggleGroup();
 
 
     public void initialize() throws FileNotFoundException {
@@ -37,6 +52,38 @@ public class TriviaGameController {
         que.parseQuestions();
 //        body.build();
 //        initGame();
+        ansA.setToggleGroup(group);
+        ansB.setToggleGroup(group);
+        ansC.setToggleGroup(group);
+        ansD.setToggleGroup(group);
+
+    }
+
+    @FXML
+    void ansAPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ansBPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ansCPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ansDPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ansBtnPressed(ActionEvent event) {
+        System.out.println(group.getSelectedToggle()==ansC);
+
+
     }
 
     @FXML
